9f3bd9ca313634eb589c32ad91c5d0605041b604
Jason DiBabbo <1293421+JasonDiBabbo@users.noreply.github.com>
2020-09-10 23:47:19 -0500

    Added TypeScript as a dev dependency and configured the StreamEventFeed widget to cycle through the latest follower and subscriber with fade animations.

--- StreamEventFeed/widget.ts
+++ StreamEventFeed/widget.ts
@@ -1,122 +1,184 @@
 /**
  * Rotating Alert Feed v1.0.0
- *
+ * 
  * - @JasonDiBabbo
  */
-var StreamEventType;
-(function (StreamEventType) {
-    StreamEventType[StreamEventType["Follower"] = 0] = "Follower";
-    StreamEventType[StreamEventType["Subscriber"] = 1] = "Subscriber";
-})(StreamEventType || (StreamEventType = {}));
-class StreamEvent {
-    constructor(eventType, iconCssClass) {
-        this.eventType = eventType;
-        this.iconCssClass = iconCssClass;
-    }
+
+interface IStreamEvent {
+    html: string;
 }
+
+enum StreamEventType {
+    Follower,
+    Subscriber
+}
+
+abstract class StreamEvent implements IStreamEvent {
+    public abstract html: string;
+
+    constructor(public eventType: StreamEventType, protected iconCssClass: string) { }
+}
+
 class FollowerEvent extends StreamEvent {
-    constructor(followerName) {
+    public html: string;
+
+    constructor(private followerName: string) {
         super(StreamEventType.Follower, 'fas fa-heart');
-        this.followerName = followerName;
+
         this.html = this.getHtml();
     }
-    getHtml() {
+
+    private getHtml(): string {
         const iconHtml = `<i class="bar-icon ${this.iconCssClass}"></i>`;
         const spanHtml = `<span class="bar-text">${this.followerName}</span>`;
         const html = `${iconHtml}${spanHtml}`;
+
         return html;
     }
 }
+
 class SubscriberEvent extends StreamEvent {
-    constructor(subscriberName) {
+    public html: string;
+
+    constructor(private subscriberName: string) {
         super(StreamEventType.Subscriber, 'fas fa-star');
-        this.subscriberName = subscriberName;
+
         this.html = this.getHtml();
     }
-    getHtml() {
+
+    private getHtml(): string {
         const iconHtml = `<i class="bar-icon ${this.iconCssClass}"></i>`;
         const spanHtml = `<span class="bar-text">${this.subscriberName}</span>`;
         const html = ` ${iconHtml}${spanHtml}`;
+
         return html;
     }
 }
+
 class EventManager {
-    static get CurrentEvent() {
+    private static _currentEventIndex = -1;
+    
+    private static _events: StreamEvent[] = [];
+
+    public static get CurrentEvent(): StreamEvent {
         if (EventManager._events.length === 0) {
             return null;
         }
+
         if (EventManager._currentEventIndex >= EventManager._events.length) {
             throw new Error(`EventManager.CurrentEvent - '_currentEventIndex' value has become out of bounds.`);
         }
+
         return EventManager._events[EventManager._currentEventIndex];
     }
-    static get NextEvent() {
+
+    public static get NextEvent(): StreamEvent {
         if (EventManager._events.length === 0) {
             return null;
         }
+
         EventManager.incrementEventIndex();
+
         if (EventManager._currentEventIndex >= EventManager._events.length) {
             throw new Error(`EventManager.NextEvent - '_currentEventIndex' value has become out of bounds.`);
         }
+
         return EventManager._events[EventManager._currentEventIndex];
     }
-    static RegisterEvent(event) {
+
+    public static RegisterEvent(event: StreamEvent): void {
         if (!event) {
             return;
         }
+
         let index = EventManager._events.findIndex((x) => x.eventType === event.eventType);
+
         if (index === -1) {
             EventManager._events.push(event);
-        }
-        else {
+        } else {
             EventManager._events.splice(index, 1, event);
         }
+
         if (EventManager._events.length === 1) {
             EventManager._currentEventIndex = 0;
         }
     }
-    static RegisterEvents(events) {
+
+    public static RegisterEvents(events: StreamEvent[]) {
         if (events) {
             events.forEach((event) => EventManager.RegisterEvent(event));
         }
     }
-    static incrementEventIndex() {
+
+    private static incrementEventIndex(): void  {
         EventManager._currentEventIndex = EventManager._currentEventIndex + 1 >= EventManager._events.length ? 0 : EventManager._currentEventIndex + 1;
     }
 }
-EventManager._currentEventIndex = -1;
-EventManager._events = [];
+
 class AnimationManager {
-    static InitializeEventCycle(timeIn, timeDisplay, timeOut) {
+    public static readonly AnimationPrefix = 'animate__';
+
+    public static readonly HiddenElementClass = 'hidden';
+
+    public static readonly CurrentBarSlideSelector = '.bar-item.slide:first-child';
+
+    public static readonly FadeInCssClasses: string[] = [
+        `${AnimationManager.AnimationPrefix}animated`,
+        `${AnimationManager.AnimationPrefix}fadeIn`
+    ];
+
+    public static readonly FadeOutCssClasses: string[] = [
+        `${AnimationManager.AnimationPrefix}animated`,
+        `${AnimationManager.AnimationPrefix}fadeOut`
+    ];
+
+    public static InitializeEventCycle(timeIn: number, timeDisplay: number, timeOut: number): void {
         AnimationManager.CycleInEvent(EventManager.CurrentEvent, timeIn, timeDisplay, timeOut);
     }
-    static CycleInEvent(event, timeIn, timeDisplay, timeOut) {
+
+    private static CycleInEvent(event: StreamEvent, timeIn: number, timeDisplay: number, timeOut: number): void {
         if (!event) {
             throw new Error(`AnimationManager.CycleInEvent - 'event' parameter is null or undefined.`);
         }
+
         AnimationManager.FadeInEvent(event, timeIn);
-        setTimeout(() => {
-            AnimationManager.FadeOutEvent(timeOut);
-            setTimeout(() => {
-                let nextEvent = EventManager.NextEvent;
-                AnimationManager.CycleInEvent(nextEvent, timeIn, timeDisplay, timeOut);
-            }, timeOut);
-        }, timeIn + timeDisplay);
+
+        setTimeout(
+            () => {
+                AnimationManager.FadeOutEvent(timeOut);
+
+                setTimeout(
+                    () => {
+                        let nextEvent = EventManager.NextEvent;
+                        AnimationManager.CycleInEvent(nextEvent, timeIn, timeDisplay, timeOut);
+                    },
+                    timeOut
+                );
+            },
+            timeIn + timeDisplay
+        );
     }
-    static FadeInEvent(event, timeIn) {
+
+    private static FadeInEvent(event: StreamEvent, timeIn: number): void {
         $(AnimationManager.CurrentBarSlideSelector)
             .html(event.html)
             .removeClass(AnimationManager.HiddenElementClass)
             .addClass(AnimationManager.FadeInCssClasses);
-        setTimeout(() => {
-            $(AnimationManager.CurrentBarSlideSelector)
-                .removeClass(AnimationManager.FadeInCssClasses);
-        }, timeIn);
+
+            setTimeout(
+                () => {
+                    $(AnimationManager.CurrentBarSlideSelector)
+                        .removeClass(AnimationManager.FadeInCssClasses);
+                },
+                timeIn
+            );
     }
-    static FadeOutEvent(timeOut) {
+
+    private static FadeOutEvent(timeOut: number): void {
         $(AnimationManager.CurrentBarSlideSelector)
             .removeClass(AnimationManager.FadeInCssClasses)
             .addClass(AnimationManager.FadeOutCssClasses);
+
         setTimeout(() => {
             $(AnimationManager.CurrentBarSlideSelector)
                 .addClass(AnimationManager.HiddenElementClass)
@@ -124,32 +186,32 @@ class AnimationManager {
         }, timeOut);
     }
 }
-AnimationManager.AnimationPrefix = 'animate__';
-AnimationManager.HiddenElementClass = 'hidden';
-AnimationManager.CurrentBarSlideSelector = '.bar-item.slide:first-child';
-AnimationManager.FadeInCssClasses = [
-    `${AnimationManager.AnimationPrefix}animated`,
-    `${AnimationManager.AnimationPrefix}fadeIn`
-];
-AnimationManager.FadeOutCssClasses = [
-    `${AnimationManager.AnimationPrefix}animated`,
-    `${AnimationManager.AnimationPrefix}fadeOut`
-];
+
+declare var $: any;
+
 window.addEventListener('onEventReceived', function (obj) {
+
 });
+
 window.addEventListener('onWidgetLoad', function (obj) {
     let data = obj["detail"]["session"]["data"];
     let fieldData = obj["detail"]["fieldData"];
+
     /* UI Parameters */
-    let timeIn = fieldData.fadeInAnimationTime == null || isNaN(fieldData.fadeInAnimationTime) ? 2000 : fieldData.fadeInAnimationTime;
-    let timeDisplay = fieldData.eventDisplayTime == null || isNaN(fieldData.eventDisplayTime) ? 4000 : fieldData.eventDisplayTime;
-    let timeOut = fieldData.fadeOutAnimationTime == null || isNaN(fieldData.fadeOutAnimationTime) ? 2000 : fieldData.fadeOutAnimationTime;
+    
+    let timeIn: number = fieldData.fadeInAnimationTime == null || isNaN(fieldData.fadeInAnimationTime) ? 2000 : fieldData.fadeInAnimationTime;
+    let timeDisplay: number = fieldData.eventDisplayTime == null || isNaN(fieldData.eventDisplayTime) ? 4000 : fieldData.eventDisplayTime;
+    let timeOut: number = fieldData.fadeOutAnimationTime == null || isNaN(fieldData.fadeOutAnimationTime) ? 2000 : fieldData.fadeOutAnimationTime;
+
     let latestFollower = data["follower-latest"];
     let latestSubscriber = data["subscriber-latest"];
-    let latestFollowerEvent = new FollowerEvent(latestFollower.name);
-    let latestSubscriberEvent = new SubscriberEvent(latestSubscriber.name);
-    let events = [latestFollowerEvent, latestSubscriberEvent];
+
+    let latestFollowerEvent: FollowerEvent = new FollowerEvent(latestFollower.name);
+    let latestSubscriberEvent: SubscriberEvent = new SubscriberEvent(latestSubscriber.name);
+
+    let events: StreamEvent[] = [latestFollowerEvent, latestSubscriberEvent];
     EventManager.RegisterEvents(events);
+
     $(AnimationManager.CurrentBarSlideSelector).prop('--animate-duration', '2s');
     AnimationManager.InitializeEventCycle(timeIn, timeDisplay, timeOut);
 });
